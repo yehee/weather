148a029b49b0b0d133206f092dc359d405130659
alice-0-kim <aliceykim0828@gmail.com>
2018-06-28 14:03:53 -0700

    Initial commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,92 @@
+<template>
+  <div class="weather-forcast">
+    <!-- <img :src="logo"> -->
+    <img :src="url">
+    <p>Latitude: {{ lat }}</p>
+    <p>Longitude: {{ lon }}</p>
+    <button @click="sendData()">How's the weather like now in Vancouver?</button>
+    <p>Today's temperature is {{ temp }} {{ units }}</p>
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+
+// var t1 = new Date()
+// var t2 = new Date()
+// t2.setHours(t2.getHours() + 1)
+// console.log(t1.toISOString())
+// console.log(t2.toISOString())
+
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      url: require('../assets/logo.png'),
+      lat: '',
+      lon: '',
+      temp: '',
+      units: ''
+    }
+  },
+  //   mounted() {
+  //     axios({
+  //       method: 'GET',
+  //       url: 'https://httpbin.org/ip' }).then(result => {
+  //       this.ip = result.data.origin;
+  //     }, error => {
+  //       console.error(error);
+  //     });
+  //   },
+  methods: {
+    sendData () {
+      axios({
+        method: 'POST',
+        'url': 'https://api2.climacell.co/v2/realtime',
+        'data': {
+          'geocode': {
+            'lon': -123.120738,
+            'lat': 49.282729
+          },
+          'location_id': '',
+          'fields': [ {
+            'name': 'temp',
+            'units': 'C'
+          } ]},
+        'headers': { 'content-type': 'application/json', 'apikey': 'NONryKyas8rLlYvCMH8OhkbhD1D9TV3Z' }
+      }).then(result => {
+        console.log(result)
+        this.lat = result.data.lat
+        this.lon = result.data.lon
+        this.temp = result.data.temp.value
+        if (this.temp > 20) {
+          this.url = require('../assets/sun.png')
+        } else {
+          this.url = require('../assets/rain.png')
+        }
+        this.units = result.data.temp.units
+      }, error => {
+        console.error(error)
+      })
+    }
+  }
+}
+</script>
+
+<!-- Add 'scoped' attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
