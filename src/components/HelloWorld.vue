1d370e57d61b4619a2c049e20dd9d3eb33e82d8a
alice-0-kim <aliceykim0828@gmail.com>
2018-06-29 15:46:16 -0700

    Update main icon; collect lat&lon input

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -4,7 +4,10 @@
     <img :src="url">
     <p>Latitude: {{ lat }}</p>
     <p>Longitude: {{ lon }}</p>
-    <button @click="sendData()">How's the weather like now in Vancouver?</button>
+    <button class="btn-v" @click="sendRealtimeData()">How's the weather like in Vancouver?</button>
+    <button class="btn-u" @click="sendForecastData()">How's the weather like in your location?</button>
+    <input type="text" v-model="latitude" placeholder="Your latitude">
+    <input type="text" v-model="longitude" placeholder="Your longitude">
     <p>Today's temperature is {{ temp }} {{ units }}</p>
   </div>
 </template>
@@ -22,9 +25,11 @@ export default {
   name: 'HelloWorld',
   data () {
     return {
-      url: require('../assets/logo.png'),
+      url: require('../assets/flower.png'),
       lat: '',
       lon: '',
+      latitude: '',
+      longitude: '',
       temp: '',
       units: ''
     }
@@ -39,7 +44,7 @@ export default {
   //     });
   //   },
   methods: {
-    sendData () {
+    sendRealtimeData () {
       axios({
         method: 'POST',
         'url': 'https://api2.climacell.co/v2/realtime',
@@ -55,7 +60,35 @@ export default {
           } ]},
         'headers': { 'content-type': 'application/json', 'apikey': 'NONryKyas8rLlYvCMH8OhkbhD1D9TV3Z' }
       }).then(result => {
-        console.log(result)
+        this.lat = result.data.lat
+        this.lon = result.data.lon
+        this.temp = result.data.temp.value
+        if (this.temp > 20) {
+          this.url = require('../assets/sun.png')
+        } else {
+          this.url = require('../assets/rain.png')
+        }
+        this.units = result.data.temp.units
+      }, error => {
+        console.error(error)
+      })
+    },
+    sendForecastData () {
+      axios({
+        method: 'POST',
+        'url': 'https://api2.climacell.co/v2/realtime',
+        'data': {
+          'geocode': {
+            'lon': Number(this.longitude),
+            'lat': Number(this.latitude)
+          },
+          'location_id': '',
+          'fields': [ {
+            'name': 'temp',
+            'units': 'C'
+          } ]},
+        'headers': { 'content-type': 'application/json', 'apikey': 'NONryKyas8rLlYvCMH8OhkbhD1D9TV3Z' }
+      }).then(result => {
         this.lat = result.data.lat
         this.lon = result.data.lon
         this.temp = result.data.temp.value
@@ -89,6 +122,10 @@ li {
 a {
   color: #42b983;
 }
+input[type="text"] {
+  border: none;
+  border-bottom: 1px solid seagreen;
+}
 button {
   padding: 7px 14px;
   border-radius: 5px;
@@ -97,9 +134,22 @@ button {
   font-size: medium;
   color: #2c3e50;
   cursor: pointer;
+  display: block;
+  margin: 0 auto;
+  transition: background 0.5s;
 }
-button:hover {
+.btn-v {
+  background: #d3f2ff;
+}
+.btn-v:hover {
   background: #b1dbed;
+}
+.btn-u {
+  margin: 10px auto;
+  background: #e3ddff;
+}
+.btn-u:hover {
+  background: #c8c3e0;
 }
 * {
   outline: none;
